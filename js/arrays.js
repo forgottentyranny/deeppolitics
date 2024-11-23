f235601570e53316f8a9c95de074dddb55f6feb2
forgottentyranny <forgottentyranny@gmail.com>
2024-11-23 18:21:34 +0100

    Update arrays.js

--- js/arrays.js
+++ js/arrays.js
@@ -1,1 +1,21 @@
-
+class Tensor {
+  constructor(params) {
+    if (isArray(params)) {
+      if (params.hasOwnProperty("rank")) {
+        this.rank = params.rank;
+      }
+      else {
+        throw "Rank not provided with the parameters in Tensor constructor.";
+      }
+      if (params.hasOwnProperty("size")) {
+        this.size = params.size;
+      }
+      if (params.hasOwnProperty("data")) {
+        this.data = params.data;
+      }
+    }
+    else {
+      throw "Parameters were not entered in an array in Tensor constructor.";
+    }
+  }
+};
